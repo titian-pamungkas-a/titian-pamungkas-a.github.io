16a2d7a434c17f84b521d586924328d75cae42d7
titian-pamungkas-a <pamungkastitian@gmail.com>
2024-09-05 20:53:42 +0700

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}, ."],
+  theme: {
+    // extend: {},
+    fontFamily:{
+      pixelify:['asd'],
+      pixel:['dsa'],
+      pixelify2:['dpcomic']
+    }
+  },
+  plugins: [],
+}
+
